ee7fd5295ac24da4b15f5d360d1b4d5e7ec8f4cb
DeepanshuGoel1122 <72638651+DeepanshuGoel1122@users.noreply.github.com>
2022-10-15 14:24:26 +0530

    Add files via upload

---
+++ WorkingCode/code11__TillRecommended/src/screen/watchscreen/WatchScreen.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import { Container,Row, Col } from 'react-bootstrap'
+import Header from '../../components/header/Header'
+import Sidebar from '../../components/sidebar/Sidebar'
+import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import './_watchscreen.scss' 
+import VideoMetaData from '../../components/videoMetaData/VideoMetaData';
+import VideoHorizontal from '../../components/videoHorizontal/VideoHorizontal';
+import Comments from '../../components/comments/Comments';
+import { getRelatedVideos, getVideoById } from '../../redux/actions/videos.action';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
+const WatchScreen = () => {
+
+
+  const {accessToken,loading} = useSelector(state=>state.auth);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!loading && !accessToken){
+      navigate('/auth')
+    }
+  
+    
+  }, [accessToken, loading, navigate])
+
+const [sidebar, toggleSidebar] = useState(false)
+  const handleToggleSidebar = () => toggleSidebar(value => !value)
+
+
+
+
+
+
+
+  
+const {id} = useParams();
+const dispatch = useDispatch()
+
+useEffect(() => {
+  dispatch(getVideoById(id))
+
+  dispatch(getRelatedVideos(id))
+
+}, [dispatch,id])
+
+
+const {videos,loading:relatedVideosLoading} = useSelector(state=>state.relatedVideos)
+
+const { video,loading2 } = useSelector(state => state.selectedVideo)
+
+  return (
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+
+      <div className="app__container">
+        <Sidebar sidebar={sidebar}
+          handleToggleSidebar={handleToggleSidebar} />
+        <Container fluid className="app__main">
+
+{/* -------------------------------------------------------------------------------- */}
+
+          <Row>
+            <Col lg={8}>
+
+              <div className='watchScreen__player'>
+
+              <iframe 
+              width="100%" 
+              height="100%" 
+              src={`https://www.youtube.com/embed/${id}`} 
+              title={video?.snippet?.title} 
+              frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+              </div>
+
+              {
+                !loading2?<VideoMetaData video={video} videoId={id} />:<h2>Loading...</h2>
+              }
+
+              <Comments videoId={id} totalComments={video?.statistics?.commentCount} />
+            </Col>
+
+
+            <Col lg={4}>
+              {
+                !loading ? 
+                videos?.filter(video=>video.snippet)
+                .map(video => (<VideoHorizontal video={video} key={video.id.videoId} />
+                )) :
+                <SkeletonTheme baseColor='#3c4147' highlightColor='#343a40'>
+                <Skeleton width='100%' height='130px' count={15} />
+                </SkeletonTheme>
+              }
+            </Col>
+          </Row>
+
+
+{/* ---------------------------------------------------------------------------------- */}
+
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default WatchScreen
---
+++ WorkingCode/code12_TillSearch/src/screen/searchscreen/SearchScreen.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom'
+import Header from '../../components/header/Header'
+import Sidebar from '../../components/sidebar/Sidebar'
+import { Container } from 'react-bootstrap'
+import { getVideosBySearch } from '../../redux/actions/videos.action';
+import VideoHorizontal from '../../components/videoHorizontal/VideoHorizontal';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
+const SearchScreen = () => {
+
+
+    const { accessToken, loading } = useSelector(state => state.auth);
+
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!loading && !accessToken) {
+            navigate('/auth')
+        }
+
+
+    }, [accessToken, loading, navigate])
+
+    const [sidebar, toggleSidebar] = useState(false)
+    const handleToggleSidebar = () => toggleSidebar(value => !value)
+
+
+
+
+
+
+
+    const { query } = useParams()
+    // console.log(query)
+
+
+    const dispatch = useDispatch() 
+    useEffect(() => {
+      
+        dispatch(getVideosBySearch(query))
+    }, [query,dispatch])
+
+    const {videos,loading:searchedVideo} = useSelector(state=>state.searchedVideos)
+    
+
+    return (
+
+        <>
+
+    <Header handleToggleSidebar={handleToggleSidebar} />
+
+      <div className="app__container">
+        <Sidebar sidebar={sidebar}
+          handleToggleSidebar={handleToggleSidebar} />
+        <Container fluid className="app__main">
+
+
+
+
+
+        {
+            !loading ? (
+                videos?.map(video=><VideoHorizontal video={video} key={video.id.videoId} SearchScreen/>)
+            ) : 
+            (
+                <SkeletonTheme baseColor='#3c4147' highlightColor='#343a40'>
+                <Skeleton width='100%' height='160px' count={20} />
+                </SkeletonTheme>
+            )
+        }
+
+
+
+
+    </Container>
+      </div>
+
+      </>
+
+    )
+}
+
+export default SearchScreen
